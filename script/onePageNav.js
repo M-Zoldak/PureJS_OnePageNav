99bc14bcbd5f08f2b211053507521dbd6a5e10dd
Mateusz Zoldak <matheoz8896@gmail.com>
2023-02-25 13:54:02 +0100

    readability refactor, allowed paths logic, hash update functionality

--- script/onePageNav.js
+++ script/onePageNav.js
@@ -1,53 +1,42 @@
 /**
  * @options
- * @param {string} selector - Should target navigation "a" node elements e.g. ".main-nav a"
- * @param {string} defaultActiveAnchor - Should target link which will gain class Active if none section reached / active in exactMatch
- * @param {string} navigationActiveClass - class that should be added on anchor when section reached
- * @param {string} articleActiveClass - class that should be added on section when reached
- * @param {int} changeOffset -distance from top to be reached through next element to change active item. Value given in precents (0-100)% - default (50)
- * @param {boolean} classOnAnchorTag - class will be set at Anchors - default (true)
- * @param {boolean} defaultLinkActive - if no section was reached, default settet at defaultActiveAnchor or first link from list will gain class active
+ * @param {string} navLinksSelector - selector targeting anchor tags
+ * @param {string} elementDefaultActive - selector to element, which obtain class active, if none section was reached(or active in exact match)
+ * @param {string} anchorActiveClass - class name that will be added on anchor when section reached and alternatively on parents passes inside parentsObtainingActiveClass
+ * @param {string} sectionActiveClass - class name that will be added on section when reached
+ * @param {int} changeOffset - the distance from the top of the page that the next element must be scrolled to in order to be activated. Value given in precents (0-100)% - default (50)
+ * @param {boolean} classOnAnchorTag - name of class that should be set on anchor elements and parents if given for parentsObtainingActiveClasses - default (true)
+ * @param {boolean} defaultLinkActive - determines whether first link element should be set as active.(t/f) May be combined with exact match option and/or element default active
  * @param {boolean} setClassesOnSections - if true, reached/active section will gain class active
- * @param {boolean} exactMatch - set class only if offset place is between begin and end of section else default link or none
- * @param {array} parentsObtainingActiveClass - array with selectors of closest(selector) nodes, where class 'active' should be added or removed on change
- * @param {array} allowedPaths - List of paths, where script will be evaluated e.g ['', '/', '/start'], default empty - evaluated on every site
- * @param {array} onChange - array of functions, that should be fired on change fe. [function1, function2], function becomes two parameters: callback(activeArticle, previousArticle)
- * @param {boolean} debugLine - debug line
+ * @param {boolean} exactMatch - set class only if offset is located between begin and end of section
+ * @param {boolean} updateHash - update hash accordingly to section
+ * @param {boolean} saveHashBetweenSections - hash won't change if there's a gap between sections, and exact match is on
+ * @param {array} parentsObtainingActiveClass - array of selectors for closest parent elements, where class 'active' should be added or removed when section changes
+ * @param {array} allowedPaths - list of paths, where script will be evaluated e.g ['', '/', '/start'], currently only simple strings, default empty - evaluated on every site
+ * @param {array} onChange - array of functions, that should be fired on change e.g. [function1, function2], callback function receives two parameters: callback(currentSection, previousSection)
+ * @param {boolean} debugLine - show debug line
  */
 class onePageNav {
-    defaultLinkActive;
-    defaultActiveAnchor;
-    differentActiveAnchor;
-    classOnAnchorTag;
-    changeOffset;
-    parentsObtainingActiveClass;
-    setClassesOnSections;
-    exactMatch;
-    allowedPaths;
-    debugLine;
-
-    navigationActiveClass;
-    articleActiveClass;
-
-    previousArticle;
-    currentArticle;
-
-    linksInNav;
-    articles;
-
-    debugLines;
-    currentWindowScrollListener;
-
-    onChange;
-
-    constructor({ selector = "nav a", defaultLinkActive = true, classOnAnchorTag = true, changeOffset = 50, parentsObtainingActiveClass = [], setClassesOnSections = false, debugLine = false, exactMatch = false, allowedPaths = undefined, navigationActiveClass = "active", articleActiveClass = "active", onChange = [], differentActiveAnchor = undefined } = {}) {
-        this.linksInNav = [...document.querySelectorAll(selector)];
-
-        if (!this.linksInNav.length) {
-            console.error("No possible selectors were found. - `" + this.selector + "`");
-            return;
-        }
-
+    // prettier-ignore
+    constructor({ 
+        navLinksSelector = "nav a", 
+        defaultLinkActive = true, 
+        classOnAnchorTag = true, 
+        changeOffset = 50, 
+        parentsObtainingActiveClass = [], 
+        setClassesOnSections = false, 
+        debugLine = false, 
+        exactMatch = false, 
+        allowedPaths = undefined, 
+        anchorActiveClass = "active", 
+        sectionActiveClass = "active", 
+        differentActiveAnchor = undefined, 
+        updateHash = false, 
+        saveHashBetweenSections = true,
+        onChange = [], 
+    } = {}) {
+            
+        this.navLinksSelector = navLinksSelector;
         this.defaultLinkActive = defaultLinkActive;
         this.classOnAnchorTag = classOnAnchorTag;
         this.changeOffset = changeOffset;
@@ -56,32 +45,49 @@ class onePageNav {
         this.debugLine = debugLine;
         this.exactMatch = exactMatch;
         this.allowedPaths = allowedPaths;
-        this.navigationActiveClass = navigationActiveClass;
-        this.articleActiveClass = articleActiveClass;
+        this.anchorActiveClass = anchorActiveClass;
+        this.sectionActiveClass = sectionActiveClass;
         this.differentActiveAnchor = differentActiveAnchor;
+        this.updateHash = updateHash;
         this.onChange = onChange;
+        this.saveHashBetweenSections = saveHashBetweenSections;
 
-        this.articles = [];
+        this.sections = [];
         this.debugLines = [];
 
-        this.invoke();
+        this.initialize();
     }
 
-    set = (optionName, value) => {
-        this[optionName] = value;
-        this.newlyInitialized = true;
-        this.invoke();
+    /**
+     * @description use to change script behavior and initialize with new values
+     * @param {string} option
+     * @param {string} value
+     */
+    set = (option, value) => {
+        this[option] = value;
+        this.refresh();
     };
 
-    invoke = () => {
-        // Initialisiert sich nur auf Hauptseite - es ist nicht ganz so gute Idee(/start, /startseite)
-        if (this.allowedPaths && !this.allowedPaths.includes(document.location.pathname)) return;
+    /**
+     * @description initializes values once again
+     */
+    refresh = () => {
+        this.newlyInitialized = true;
+        this.initialize();
+    };
 
-        this.handleDefaultLinkActive();
+    initialize = () => {
+        if (this.shouldTerminate()) return;
 
-        this.articles = this.findRelatedArticles();
-        if (!this.articles.length) {
-            console.warn("No sections found");
+        this.findLinks();
+        if (!this.linksInNav) {
+            console.debug("The selector did not match any elements - `" + this.navLinksSelector + "`");
+            return;
+        }
+
+        this.findRelatedSections();
+        if (!this.sections) {
+            console.debug("No sections found");
             return;
         }
 
@@ -90,9 +96,23 @@ class onePageNav {
         this.handleOutput();
     };
 
-    findRelatedArticles = () => {
+    shouldTerminate = () => {
+        if (!this.allowedPaths) return false;
+
+        let currentPath = document.location.pathname.trim();
+
+        let pathMatchesAllowedPaths = this.allowedPaths.some((el) => currentPath.match(el.trim()));
+
+        return !pathMatchesAllowedPaths;
+    };
+
+    findLinks = () => {
+        this.linksInNav = [...document.querySelectorAll(this.navLinksSelector)];
+    };
+
+    findRelatedSections = () => {
         let hashes = this.linksInNav.map((el) => el.hash).filter((el) => el);
-        return [...document.querySelectorAll(hashes.join(", "))];
+        this.sections = [...document.querySelectorAll(hashes.join(", "))];
     };
 
     handleScrollListener = () => {
@@ -103,32 +123,37 @@ class onePageNav {
     };
 
     handleOutput = () => {
-        this.findCurrentArticle();
+        let foundSection = this.findCurrentSection();
 
-        if (this.articleChanged() || this.newlyInitialized) {
+        this.updateCurrentSection(foundSection);
+
+        if (this.sectionChanged() || this.newlyInitialized) {
             this.newlyInitialized = false;
 
             this.clearClasses();
             this.handleDefaultLinkActive();
 
+            this.handleHash();
             this.handleCallbacks();
 
-            if (this.currentArticle == undefined) return;
+            if (this.noSectionActive()) return;
 
-            this.handleArticleClasses();
+            this.handleSectionClasses();
             this.addActiveClassesOnNavigationLinks();
         }
     };
 
-    findCurrentArticle = () => {
-        this.previousArticle = this.currentArticle;
-        let tempCurrent = this.articles.filter((el) => el.offsetTop < window.scrollY + window.innerHeight / (100 / this.changeOffset)).at(-1);
+    findCurrentSection = () => {
+        return this.sections.filter((section) => this.isBelowSectionOffsetTop(section)).at(-1);
+    };
 
-        if (!this.exactMatch) this.currentArticle = tempCurrent;
-        else if (tempCurrent) {
-            this.currentArticle = tempCurrent.offsetHeight + tempCurrent.offsetTop > window.scrollY + window.innerHeight / (100 / this.changeOffset) ? tempCurrent : this.handleDefaultLinkActive();
+    updateCurrentSection = (newSection) => {
+        this.previousSection = this.currentSection;
+
+        if (!this.exactMatch) {
+            this.currentSection = newSection ?? this.handleDefaultLinkActive();
         } else {
-            this.currentArticle = undefined;
+            this.currentSection = this.isAboveSectionOffsetBottom(newSection) ? newSection : this.handleDefaultLinkActive();
         }
     };
 
@@ -137,61 +162,118 @@ class onePageNav {
             this.removeActiveClass(link);
         });
 
-        this.articles.forEach((el) => {
-            el.classList.remove(this.articleActiveClass);
+        this.sections.forEach((el) => {
+            el.classList.remove(this.sectionActiveClass);
         });
     };
 
     handleDefaultLinkActive = () => {
-        if (this.differentActiveAnchor) this.defaultActiveAnchor = document.querySelector(this.differentActiveAnchor);
-        if (this.currentArticle == undefined && this.defaultLinkActive) {
-            this.addActiveClass(this.defaultActiveAnchor ?? this.linksInNav[0]);
+        if (this.differentActiveAnchor) {
+            this.elementDefaultActive = document.querySelector(this.differentActiveAnchor);
+        }
+        if (this.noSectionActive() && this.defaultLinkActive) {
+            this.addActiveClass(this.elementDefaultActive ?? this.linksInNav[0]);
+        }
+    };
+
+    handleHash = () => {
+        if (!this.updateHash) {
+            return;
+        } else if (this.exactMatch && !this.currentSection && this.saveHashBetweenSections && this.insideWorkingArea()) {
+            let lastSectionAbove = this.findCurrentSection();
+            history.pushState({}, "", lastSectionAbove ? "#" + lastSectionAbove.id : " ");
+        } else {
+            history.pushState({}, "", this.currentSection ? "#" + this.currentSection.id : " ");
         }
     };
 
     handleCallbacks = () => {
         this.onChange.forEach((callback) => {
-            callback(this.currentArticle, this.previousArticle);
+            callback(this.currentSection, this.previousSection);
         });
     };
 
-    handleArticleClasses = () => {
+    handleSectionClasses = () => {
         if (this.setClassesOnSections) {
-            this.currentArticle.classList.add(this.articleActiveClass);
+            this.currentSection.classList.add(this.sectionActiveClass);
         }
     };
 
     addActiveClassesOnNavigationLinks = () => {
-        let currentlyActiveLinks = this.findLinksWithHashEqualToCurrentArticleId(this.linksInNav, this.currentArticle.id);
+        let currentlyActiveLinks = this.findLinksWithHashEqualTocurrentSectionId(this.linksInNav, this.currentSection.id);
         currentlyActiveLinks.forEach((activeLink) => this.addActiveClass(activeLink));
     };
 
-    findLinksWithHashEqualToCurrentArticleId = (links, hash) => {
+    findLinksWithHashEqualTocurrentSectionId = (links, hash) => {
         return links.filter((el) => el.hash.includes(hash));
     };
 
     addActiveClass = (el) => {
         if (this.classOnAnchorTag) {
-            el.classList.add(this.navigationActiveClass);
+            el.classList.add(this.anchorActiveClass);
         }
 
         this.parentsObtainingActiveClass.forEach((parentSelector) => {
-            el.closest(parentSelector).classList.add(this.navigationActiveClass);
+            el.closest(parentSelector).classList.add(this.anchorActiveClass);
         });
     };
 
     removeActiveClass = (el) => {
         if (this.classOnAnchorTag) {
-            el.classList.remove(this.navigationActiveClass);
+            el.classList.remove(this.anchorActiveClass);
         }
 
         this.parentsObtainingActiveClass.forEach((parentSelector) => {
-            el.closest(parentSelector).classList.remove(this.navigationActiveClass);
+            el.closest(parentSelector).classList.remove(this.anchorActiveClass);
         });
     };
 
-    articleChanged = () => {
-        return this.currentArticle != this.previousArticle;
+    sectionChanged = () => {
+        return this.currentSection != this.previousSection;
+    };
+
+    noSectionActive = () => {
+        return this.currentSection == undefined;
+    };
+
+    currentOffset = () => {
+        return window.scrollY + window.innerHeight / (100 / this.changeOffset);
+    };
+
+    insideWorkingArea = () => {
+        return this.currentOffset() > this.sections.at(0).offsetTop;
+    };
+
+    isExactlyInsideSection = (section) => {
+        return this.isBelowSectionOffsetTop(section) && this.isAboveSectionOffsetBottom(section);
+    };
+
+    isAboveSectionOffsetTop = (section) => {
+        if (!section) {
+            return false;
+        }
+        return this.currentOffset() < section.offsetTop;
+    };
+
+    isAboveSectionOffsetBottom = (section) => {
+        if (!section) {
+            return false;
+        }
+        return this.currentOffset() < section.offsetTop + section.offsetHeight;
+    };
+
+    isBelowSectionOffsetTop = (section) => {
+        if (!section) {
+            return false;
+        }
+        return this.currentOffset() > section.offsetTop;
+    };
+
+    isBelowSectionOffsetBottom = (section) => {
+        if (!section) {
+            return false;
+        }
+        return this.currentOffset() > section.offsetTop + section.offsetHeight;
     };
 
     handleDebugLine = () => {
